6ce663c4adc0dbcca41c8463b2cdfec5d1b4b649
BhanuPrakash Parumanchala <64313025+bhanup6@users.noreply.github.com>
2025-03-07 10:23:38 -0500

    Create exporcert.ps1

---
+++ exporcert.ps1
@@ -0,0 +1,16 @@
+try {
+    $request = [System.Net.HttpWebRequest]::Create("https://digicert.com")
+    $response = $request.GetResponse()
+    $cert = $request.ServicePoint.Certificate  # Access from ServicePoint
+    $response.Close()
+
+    if ($cert) {
+        $bytes = $cert.Export([Security.Cryptography.X509Certificates.X509ContentType]::Cert)
+        [System.IO.File]::WriteAllBytes("C:\Users\user1\certificate_digi.cer", $bytes)
+        Write-Host "Certificate exported successfully!"
+    } else {
+        Write-Host "Error: No certificate found on ServicePoint."
+    }
+} catch {
+    Write-Host "Error: $($_.Exception.Message)"
+}
